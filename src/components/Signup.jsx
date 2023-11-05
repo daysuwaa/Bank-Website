b17618a95bdd122abbf0cd00eed71d5c4288f314
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-11-05 14:21:26 +0100

    responsive button

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -10,7 +10,7 @@ import Apple from '../assets/Icon-4.png'
 const Signup = () => {
   
   return (
-    <div className='px-9 m-0' >
+    <div className='px-7 m-0' >
      
     <div className='' style={{backgroundColor:'#1A1A1A', border: '1px solid #262626' ,borderRadius: '50px'}}>
     <img src={Abstract} alt='ab' className='lg:w-[256px] lg:h-[243px]' style={{alignSelf:'stretch', width:'167px', height:'159px', position:'absolute', right:'35px', fill: 'radial-gradient(152.24% 143.75% at 103.26% 0%, rgba(25, 25, 25, 0.30) 20.29%, rgba(202, 255, 51, 0.30) 34.91%, rgba(25, 25, 25, 0.30) 60.69%)' }}/>
@@ -25,43 +25,48 @@ const Signup = () => {
     <input
       type="text"
       placeholder="Enter First Name"
-      style={{ color: '#59595A', fontFamily: 'lexend', fontSize: '16px', fontStyle: 'normal', fontWeight: '300', lineHeight: '150%', borderRadius: '88px', border: '1px solid #262626', background: '#1A1A1A', width: '320px', height: '64px', padding: '20px' }}
+      style={{ color: '#59595A', fontFamily: 'lexend', fontSize: '16px', fontStyle: 'normal', fontWeight: '300', lineHeight: '150%', borderRadius: '88px', border: '1px solid #262626', background: '#1A1A1A',  padding: '20px' }}
+      className='w-[320px] h-[58px] lg:w-[400px] lg:h-[64px]'
     />
     <input
       type="text"
       placeholder="Enter Last Name"
-      style={{ color: '#59595A', fontFamily: 'lexend', fontSize: '16px', fontStyle: 'normal', fontWeight: '300', lineHeight: '150%', borderRadius: '88px', border: '1px solid #262626', background: '#1A1A1A', width: '320px', height: '64px', padding: '20px' }}
+      style={{ color: '#59595A', fontFamily: 'lexend', fontSize: '16px', fontStyle: 'normal', fontWeight: '300', lineHeight: '150%', borderRadius: '88px', border: '1px solid #262626', background: '#1A1A1A',  padding: '20px' }}
+      className='w-[320px] h-[58px] lg:w-[400px] lg:h-[64px]'
     />
     <input
       type="text"
       placeholder="Enter your Email"
-      style={{ color: '#59595A', fontFamily: 'lexend', fontSize: '16px', fontStyle: 'normal', fontWeight: '300', lineHeight: '150%', borderRadius: '88px', border: '1px solid #262626', background: '#1A1A1A', width: '320px', height: '64px', padding: '20px' }}
+      style={{ color: '#59595A', fontFamily: 'lexend', fontSize: '16px', fontStyle: 'normal', fontWeight: '300', lineHeight: '150%', borderRadius: '88px', border: '1px solid #262626', background: '#1A1A1A',  padding: '20px' }}
+      className='w-[320px] h-[58px] lg:w-[400px] lg:h-[64px]'
     />
     <div>
     <input
       type="text"
       placeholder="Enter your password"
-      style={{ color: '#59595A', fontFamily: 'lexend', fontSize: '16px', fontStyle: 'normal', fontWeight: '300', lineHeight: '150%', borderRadius: '88px', border: '1px solid #262626', background: '#1A1A1A', width: '320px', height: '64px', padding: '20px' }}
+      style={{ color: '#59595A', fontFamily: 'lexend', fontSize: '16px', fontStyle: 'normal', fontWeight: '300', lineHeight: '150%', borderRadius: '88px', border: '1px solid #262626', background: '#1A1A1A',  padding: '20px' }}
+      className='w-[320px] h-[58px] lg:w-[400px] lg:h-[64px]'
     />
     </div>
   </div>
 </div>
 {/* sign up and log in buttons */}
-<div style={{display:'grid', padding:'14px 24px', alignItems:'center', gap:'10px', alignSelf:'stretch' }}className="justify-center items-center">
-  <button style={{borderRadius:'63px', background:'#CAFF33', width:'404px', height:'49px'}}>
-    <Link to='/bank/signup' style={{color:'#262626', textAlign:'center', fontFamily:'lexend', fontSize:'14px', fontWeight:'400', lineHeight:'150%'}}>
+<div style={{display:'grid', padding:'14px 24px', alignItems:'center', gap:'10px', alignSelf:'stretch' }}className="justify-center items-center ">
+  <button style={{borderRadius:'63px', background:'#CAFF33'}} className=' w-[390px] h-[49px] lg:w-[404px] lg:h-[49px]'>
+    <Link to='/bank/signup' style={{color:'#262626', textAlign:'center', fontFamily:'lexend', fontSize:'14px', fontWeight:'400', lineHeight:'150%'}} >
     Sign Up
     </Link>
+    
   </button>
-  <button style={{borderRadius:'63px', border: '1px solid var(--grey-20, #333)', background:'#262626', width:'404px', height:'49px', marginTop:'10px'}}>
+  <button style={{borderRadius:'63px', border: '1px solid var(--grey-20, #333)', background:'#262626', marginTop:'10px'}} className=' w-[390px] h-[49px] lg:w-[404px] lg:h-[49px]'>
     <Link to='/bank/login' style={{color:'white', textAlign:'center', fontFamily:'lexend', fontSize:'14px', fontWeight:'400', lineHeight:'150%'}}>
     Login
    </Link>
   </button>
 </div>
 
-  <div className="flex items-center ml-[3rem] lg:ml-[33rem]">
-      <div style={{width:'129px', height:'1px', background: '#B3B3B3'}}></div>
+  <div className="flex items-center ml-[2rem] lg:ml-[31rem]">
+      <div style={{width:'110px', height:'1px', background: '#B3B3B3'}}></div>
       <div className="mx-2 text-white">Or continue with</div>
       <div style={{width:'129px', height:'1px', background: '#B3B3B3'}}></div>
     </div>
