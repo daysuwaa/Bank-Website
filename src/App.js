4b96a4e3a05d12046db7c82f07955da08285ed03
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-11-05 04:49:51 +0100

    created components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import Home from './components/Home';
+import About from './components/About';
+import Careers from './components/Careers';
+import Security from './components/Security';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Navbar from './components/Navbar'
+import ErrorPage from './components/ErrorPage';
+
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+    <Navbar/>
+      <Routes>
+        <Route path="/bank/" element={<Home />} />
+        <Route path="/bank/about" element={<About />} />
+        <Route path="/bank/careers" element={ <Careers/>} />
+        <Route path="/bank/security" element={<Security />} />
+        <Route path="/bank/login" element={<Login />} />
+        <Route path="/bank/signup" element={<Signup />} />
+        <Route path="*" element={<ErrorPage />} />
+       </Routes>
+    </Router>
   );
 }
 
