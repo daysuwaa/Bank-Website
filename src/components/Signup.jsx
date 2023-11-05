b9a6a41c5a123878889aaff51ef419e847382cad
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-11-05 23:24:35 +0100

    testimony side

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import Abstract from'../assets/Abstract Design.png'
-// import Omo from'../assets/Icon.png'
+import Apos from'../assets/Icon-14.png'
 import Gmail from'../assets/Icon-2.png'
 import Facebook from '../assets/Icon-3.png'
 import Apple from '../assets/Icon-4.png'
@@ -11,7 +11,6 @@ const Signup = () => {
   
   return (
     <div className='px-7 m-0' >
-     
     <div className='' style={{backgroundColor:'#1A1A1A', border: '1px solid #262626' ,borderRadius: '50px'}}>
     <img src={Abstract} alt='ab' className='lg:w-[256px] lg:h-[243px]' style={{alignSelf:'stretch', width:'167px', height:'159px', position:'absolute', right:'35px', fill: 'radial-gradient(152.24% 143.75% at 103.26% 0%, rgba(25, 25, 25, 0.30) 20.29%, rgba(202, 255, 51, 0.30) 34.91%, rgba(25, 25, 25, 0.30) 60.69%)' }}/>
      <div style={{display:'flex', flexDirection:'column', alignItems:'flex-start', gap:'60px'}} className='px-[170px] py-[50px] lg:px-[220px] lg:py-[80px]'/>
@@ -76,13 +75,56 @@ const Signup = () => {
       <img src={Gmail} alt='gmail' style={{display:'flex', padding:'14px', borderRadius:'50px', border:'1px solid #CAFF33', background: 'linear-gradient(180deg, rgba(202, 255, 51, 0.10) -66.22%, rgba(202, 255, 51, 0.00) 85.46%'}}/>
       <img src={Facebook} alt='facebook'style={{display:'flex', padding:'14px', borderRadius:'50px', border:'1px solid #CAFF33', background: 'linear-gradient(180deg, rgba(202, 255, 51, 0.10) -66.22%, rgba(202, 255, 51, 0.00) 85.46%'}}/>
       <img src={Apple} alt='apple' style={{display:'flex', padding:'14px', borderRadius:'50px', border:'1px solid #CAFF33', background: 'linear-gradient(180deg, rgba(202, 255, 51, 0.10) -66.22%, rgba(202, 255, 51, 0.00) 85.46%'}}/>
-
     </div>
-
 
 
      </div>
+     
     </div>
+    {/* testimonalsss */}
+
+    <h1 style={{color:' #FFF',fontFamily: 'Lexend',fontStyle: 'normal',fontWeight: '500',lineHeight: '57px,', padding:'3rem  6rem 0'}} className='text-3xl lg:text-5xl'>
+    Our 
+   <span style={{color: 'var(--green-60, #CAFF33)',fontFamily: 'Lexend',fontStyle: 'normal',fontWeight: '500',lineHeight: '58px'}}> Testimonials </span> </h1>
+   <div>
+   <p style={{color: 'var(--grey-70, #B3B3B3)',fontFamily: 'Lexend',fontStyle: 'normal',fontWeight: '300' }} className='text-md lg:text-lg  text-center '>
+    Discover how YourBank has transformed lives with innovative digital solutions and personalized customer service. See why our clients trust us for a secure and prosperous financial journey
+  </p>
+
+
+<div className='p-5'>
+  <div style={{display:'flex', padding:'12px', alignItems:'flex-start', borderRadius: '82px',border: '1px solid var(--grey-15, #262626)' }}className="justify-center items-center ">
+  <button style={{borderRadius:'140px', padding:'10px 18px', background:'#CAFF33', justifyContent:'center', alignItems:'center', gap:'10px'}} className=' w-[100px] h-[39px] lg:w-[404px] lg:h-[49px]'>
+    <Link to='/bank/signup' style={{color:'#262626', textAlign:'center', fontFamily:'lexend', fontSize:'9px', fontWeight:'400', lineHeight:'150%'}} >
+    For Individuals
+    </Link>
+    
+  </button>
+  <button style={{borderRadius:'140px', padding:'10px 18px', justifyContent:'center', alignItems:'center', gap:'10px' , border: '1px solid var(--grey-15, #262626)'}} className=' w-[100px] h-[48px] lg:w-[404px] lg:h-[49px]'>
+    <Link to='/bank/login' style={{color:'white', textAlign:'center', fontFamily:'lexend', fontSize:'9px', fontWeight:'400'}}>
+    For Business
+   </Link>
+  </button>
+</div>
+</div>
+
+<div className="grid lg:grid-cols-3 lg:grid-rows-1  gap-4 grid-cols-1 grid-rows-1 md:grid-cols-2 md:grid-rows-2" style={{ padding: '20px', gap: '10px', alignItems: 'center' }}>
+<img src={Apos} alt='app' className='justify-center items-center w-[44px] h-[44px] ' style={{}}/>
+<p style={{background: 'linear-gradient(270deg, rgba(25, 25, 25, 0.00) 0%, #191919 105.84%)', color:'var(--absolute-white, #FFF)', textAlign:'center', fontFamily:'Lexend', fontSize:'14px', fontStyle:'normal'}}>
+I recently started my own business, and YourBank has been instrumental in helping me set up my business accounts and secure the financing I needed. Their expert guidance and tailored solutions have been invaluable.
+</p>
+
+<img src={Apos} alt='app' />
+<p style={{background: 'linear-gradient(270deg, rgba(25, 25, 25, 0.00) 0%, #191919 105.84%)', color:'var(--absolute-white, #FFF)', textAlign:'center', fontFamily:'Lexend', fontSize:'14px', fontStyle:'normal'}}>
+  I recently started my own business, and YourBank has been instrumental in helping me set up my business accounts and secure the financing I needed. Their expert guidance and tailored solutions have been invaluable.
+  </p>
+<img src={Apos} alt='app'/>
+<p style={{background: 'linear-gradient(270deg, rgba(25, 25, 25, 0.00) 0%, #191919 105.84%)', color:'var(--absolute-white, #FFF)', textAlign:'center', fontFamily:'Lexend', fontSize:'14px', fontStyle:'normal'}}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium aperiam odio ipsa, quis laudantium laborum, optio, reprehenderit facere eum similique at accusantium voluptas voluptate? Quos animi tenetur sunt totam mollitia.
+  </p>
+</div>
+
+
+  </div>
     </div>
   )
 }
