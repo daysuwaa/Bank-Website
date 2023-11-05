65c12b7c74e9d1c2c161d2e8dce70a2439dcd3b1
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-11-05 06:04:04 +0100

    edited the font

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from 'react';
+import Shape from '../assets/shape-30.png';
+import Bank from '../assets/YourBanK.png';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="rounded-full border border-gray-800 bg-grey-800 mt-3">
+      <div className="flex items-center justify-between w-screen p-4">
+        {/* Images */}
+        <div className="flex items-center"> {/* Group the images together */}
+          <img className='w-[30px] h-[30px]' src={Shape} alt='logo' />
+          <img className='w-[83.75px] h-[13.75px] flex-shrink-0' src={Bank} alt='logo' />
+        </div>
 
-export default Navbar
+        {/* Navs (centered) */}
+        <ul className="flex space-x-2 font-medium text-white text-base font-lexend leading-7">
+          <li>
+            <Link to="/bank/" className='font-lexend'>
+              Home
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/bank/" className='font-lexend'>
+              Careers
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/bank/" className='font-lexend'>
+              About
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/bank/" className='font-lexend'>
+              Security
+            </Link>
+          </li>
+        </ul>
+
+        {/* P tags (side by side) */}
+        <div className="flex space-x-2">
+          <Link to="/bank/signup" className=''>
+            Sign Up
+          </Link>
+          <button>
+          <Link to="/bank/signup" className=''>
+            Login
+          </Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",
 ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        lexend:[ 'Lexend', 'sans-serif;']
+    },
   },
   plugins: [],
+}
 }
