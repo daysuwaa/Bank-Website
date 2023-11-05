ef9dcbf48b8afeceb53a9066bcd92990c0978af0
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-11-05 08:51:10 +0100

    Updates

--- src/components/ErrorPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ErrorPage = () => {
-  return (
-    <div>Errorpage</div>
-  )
-}
-
-export default ErrorPage
--- src/components/Security.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Security = () => {
-  return (
-    <div>Security</div>
-  )
-}
-
-export default Security
