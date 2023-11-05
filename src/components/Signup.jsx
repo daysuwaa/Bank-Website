6d9ac2a49b1eb6043c5340556d81624cd0acc772
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-11-05 18:02:58 +0100

    edited somethings

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -14,14 +14,14 @@ const Signup = () => {
      
     <div className='' style={{backgroundColor:'#1A1A1A', border: '1px solid #262626' ,borderRadius: '50px'}}>
     <img src={Abstract} alt='ab' className='lg:w-[256px] lg:h-[243px]' style={{alignSelf:'stretch', width:'167px', height:'159px', position:'absolute', right:'35px', fill: 'radial-gradient(152.24% 143.75% at 103.26% 0%, rgba(25, 25, 25, 0.30) 20.29%, rgba(202, 255, 51, 0.30) 34.91%, rgba(25, 25, 25, 0.30) 60.69%)' }}/>
-     <div style={{display:'flex', padding:'80px 200px', flexDirection:'column', alignItems:'flex-start', gap:'60px'}}/>
+     <div style={{display:'flex', flexDirection:'column', alignItems:'flex-start', gap:'60px'}} className='px-[170px] py-[50px] lg:px-[220px] lg:py-[80px]'/>
      <div style={{borderRadius:'16px', backgroundBlendMode:'overlay, normal'}}>
      <h1 style={{color:'#CAFF33', textAlign:'center', fontStyle:'normal', fontWeight:'500', lineHeight:'normal', fontFamily:'lexend'}} className='text-3xl lg:text-4xl'>Sign Up</h1>
      <p style={{color: 'var(--grey-75, #BFBFBF)',textAlign: 'center',fontFamily: 'Lexend',fontStyle: 'normal',fontWeight: '300',lineHeight: 'normal' ,paddingTop:'9px'}} className='text-md lg:text-lg px-12'>
       Join our community today! Create an account to unlock exclusive features and personalized experiences.</p>
 
       <div className="flex justify-center items-center h-full">
-  <div className="grid lg:grid-cols-2 lg:grid-rows-2 gap-4 grid-cols-1 grid-rows-1 md:grid-cols-2 md:grid-rows-2" style={{ padding: '20px', gap: '10px', alignItems: 'center' }}>
+      <div className="grid lg:grid-cols-2 lg:grid-rows-2 gap-4 grid-cols-1 grid-rows-1 md:grid-cols-2 md:grid-rows-2" style={{ padding: '20px', gap: '10px', alignItems: 'center' }}>
     <input
       type="text"
       placeholder="Enter First Name"
@@ -65,10 +65,10 @@ const Signup = () => {
   </button>
 </div>
 
-  <div className="flex items-center mx-[2rem] lg:mx-[31rem] ">
-      <div style={{ height:'1px', background: '#B3B3B3'}} ></div>
-      <div className=" text-white">Or continue with</div>
-      <div style={{height:'1px', background: '#B3B3B3'}} ></div>
+  <div className="flex items-center justify-center  ">
+      <div style={{ height:'1px', background: '#B3B3B3', width:'100px'}} ></div>
+      <div className=" text-white"> Or continue with </div>
+      <div style={{height:'1px', background: '#B3B3B3', width:'100px'}} ></div>
     </div>
 
     {/* icons */}
