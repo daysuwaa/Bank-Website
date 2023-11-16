7945bb818529fa4b8a1661f1c95abe512b97895f
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-11-16 10:53:07 +0100

    App edited

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Security from './components/Security';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Navbar from './components/Navbar'
-import ErrorPage from './components/ErrorPage';
+// import ErrorPage from './components/ErrorPage';
 import Footer from './components/Footer';
 
 
@@ -16,13 +16,13 @@ function App() {
     <Router basename='/Bank-Website'>
     <Navbar/>
       <Routes>
-        <Route path="/bank/" element={<Home />} />
+        <Route path="*" element={<Home />} />
         <Route path="/bank/about" element={<About />} />
         <Route path="/bank/careers" element={ <Careers/>} />
         <Route path="/bank/security" element={<Security />} />
         <Route path="/bank/login" element={<Login />} />
         <Route path="/bank/signup" element={<Signup />} />
-        <Route path="*" element={<ErrorPage />} />
+        {/* <Route path="*" element={<ErrorPage />} /> */}
        </Routes>
        <Footer/>
     </Router>
