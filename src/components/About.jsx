c0553783caa9074df44569dd5c9ddfc1aad48c99
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-11-20 23:12:37 +0100

    Commit: New asset added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,90 @@
 import React from 'react'
+import Image2 from '../assets/Image-2.png'
+// import Image3 from '../assets/Image-3.png'
+import Image8 from '../assets/Image-8.png'
+// import Image5 from '../assets/Image-5.png'
+// import Image6 from '../assets/Image-6.png'
+// import Image7 from '../assets/Image-7.png'
+// import ImageConatiner from '../assets/Image Conatoner.png'
+// import ImageConatiner2 from '../assets/Image Container-2.png'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div style={{background: 'var(--grey-10, #1A1A1A)'}}>
+      <div className='lg:block hidden'>
+      <div style={{display:'flex', height:'648px', padding:'40px', gap:'-174px', borderRadius:'20px',  }} className="lg:block hidden">
+      <div className='grid grid-cols-2'>
+        <div className='h-[408px] w-[658px]' style={{display:'flex', flexDirection:'column', gap:'23px', padding:'60px', }}>
+        <p style={{color:'var(--absolute-white, #FFF', fontSize:'18px', fontFamily:'lexend'}}>Welcome to YourBank</p>
+        <div style={{borderRadius: '20px 0px 80px 20px', background: 'var(--grey-10, #1A1A1A)',}}>
+          <h1 style={{color:'var(--absolute-white, #FFF', fontSize:'38px', fontFamily:'lexend'}}>
+           Where Banking Meets <span style={{color:'var(--green-60, #CAFF33', fontSize:'38px', fontFamily:'lexend'}}> Excellence!</span>
+          </h1>
+          <p style={{color:'var(--grey-70, #B3B3B3', fontSize:'16px',}}>  At YourBank, we believe that banking should be more than just transactions. It should be an experience that empowers individuals and businesses to thrive and reach their financial goals. As a trusted financial institution, we are committed to delivering exceptional banking services that go beyond expectations. With a focus on innovation, personalized solutions, and unwavering integrity, we strive to provide the best banking experience for our valued customers. Join us on this exciting journey and discover a new level of banking excellence.</p>
+        </div>
+        </div>
+        <div style={{borderRadius:'16px',}}>
+          <img src={Image2} alt='man' className=']'/>
+        </div>
+      </div>
+      {/* end of grid */}
+      </div>
+    </div>  
+      {/* end of the first div */}
+
+      {/* for ss */}
+      <div className='block lg:hidden justify-center items-center text-center p-[24px]'>
+        <img src={Image2} alt='man' className='h-[253px]  mx-auto md:w-[100%]'/>
+        <p style={{color:'var(--absolute-white, #FFF', fontSize:'14px', fontFamily:'lexend', paddingTop:'1rem'}}>Welcome to YourBank</p>
+        <h1 style={{color:'var(--absolute-white, #FFF', fontSize:'28px', fontFamily:'lexend', textAlign:'center'}}>
+          
+            Where Banking<br></br> Meets<span style={{color:'var(--green-60, #CAFF33', fontSize:'28px', fontFamily:'lexend'}}> Excellence</span>
+          </h1>
+          <p style={{color:'var(--grey-70, #B3B3B3', fontSize:'14px', textAlign:'center'}}>At YourBank, we believe that banking should be more than just transactions. It should be an experience that empowers individuals and businesses to thrive and reach their financial goals. As a trusted financial institution, we are committed to delivering exceptional banking services that go beyond expectations. With a focus on innovation, personalized solutions, and unwavering integrity, we strive to provide the best banking experience for our valued customers. Join us on this exciting journey and discover a new level of banking excellence.</p>
+      </div>
+      {/* end of ss */}
+
+      {/* mission % vision */}
+      <div>
+        <div className='text-center lg:text-start pt-[2rem] px-[1.5rem]'>
+        <h1 className=' text-xl lg:text-2xl' style={{color: 'var(--green-60, #CAFF33)', fontFamily:'lexend'}}>Mission & Vison</h1>
+        <p className='text-sm lg:text-lg' style={{color: 'var(--grey-70, #B3B3B3)',}} >We envision being a leading force in the industry, driven by innovation, integrity, and inclusivity, creating a brighter financial future for individuals and businesses while maintaining a strong commitment to customer satisfaction and community development</p>
+        </div>
+
+        {/* imagesss */}
+        <div className='grid lg:grid-cols-2 lg:grid-rows-2 grid-cols-1'>
+          {/* first grid */}
+          <div>
+            <img src={Image8} alt='pix'/>
+          </div>
+          {/* 2 */}
+          <div>
+          <div style={{display:'flex', paddingLeft:'0px', flexDirection:'column', alignItems:'flex-start', gap:'20px', flex:'1 0 0', borderLeft:'1px solid var(--green-60, #CAFF33'}}>
+            <h1>Mission</h1>
+            <p>At YourBank, our mission is to empower our customers to achieve financial success. We are dedicated to delivering innovative banking solutions that cater to their unique needs. Through personalized services, expert guidance, and cutting-edge technology, we aim to build strong, lasting relationships with our customers. Our mission is to be their trusted partner, helping them navigate their financial journey and realize their dreams.</p>
+            </div>
+
+          </div>
+          {/* 3 */}
+          <div>
+
+          </div>
+          {/* 4 */}
+          <div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+ 
+
+
+    </div>
+</div>
+// end of code
   )
 }
 
