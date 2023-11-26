8a45f9ba5fdab12d0c80b59f1adff17afcfaa3f3
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2023-11-26 22:53:48 +0100

    commit:navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,7 +28,6 @@ const Navbar = () => {
               Home
             </Link>
           </li>
-
           <li>
             <Link to="/bank/careers" className='font-lexend hover:underline'>
               Careers
